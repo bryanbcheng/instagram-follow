46a6131411493e47fc15d885c9eb927081868a24
Bryan Cheng <bryan.b.cheng@gmail.com>
2016-09-01 23:42:43 +0900

    fixed express issues, added appropriate dependencies

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ app.set('port', process.env.PORT || 5000);
 app.use(express.static(__dirname + '/public'));
 
 app.set('views', __dirname + '/views');
-//app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 app.get('/', function(request, response) {
-	response.render('index.html');
+	response.render('index');
     });
 
 app.get('/privacy', function(request, response) {
@@ -201,7 +202,7 @@ function followUser(userId, stats) {
     // follow user if not followed already
 }
 
-getMedia();
+//getMedia();
 
 /*
 
